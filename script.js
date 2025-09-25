a3ab89fe29bd9fe65cf918bff10b4c8443b1944b
EWOLENRIQUEZ <ewolenriquez0@gmail.com>
2025-09-25 14:10:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const message = `You felt like sunday morning, basking in sun while sipping coffe in the porch.\n\nYou've never know how much baggage you lifted just by being there . Thank you for being you\n\nI wish for more people to know your soul. Because I want them to witness God's most beautiful creaton;
+const message = `You felt like sunday morning, basking in sun while sipping coffe in the porch.\n\nYou've never know how much baggage you lifted just by being there . Thank you for being you\n\nI wish for more people to meet your soul. So they could glimpse God's most beautiful creaton;
 
 function showLetter() {
   document.getElementById("introText").style.opacity = 0;
@@ -21,5 +21,6 @@ function showLetter() {
     typeWriter();
   }, 600);
 }
+
 
 
